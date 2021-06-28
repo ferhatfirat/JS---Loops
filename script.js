aba56efcc03944b861721520219feb7f587b44b8
ferhat <ferhatfirat.gazi@gmail.com>
2021-06-28 22:53:04 +0300

    JS - Loops

---
+++ script.js
@@ -0,0 +1,87 @@
+// While Loops
+
+// let i = 0, sum = 0;
+
+// while (i < 6) {
+//     sum = sum + i;
+//     i++;
+// }
+// console.log("Bactick is nice");
+// console.log(`The total is: ${sum}`);
+
+// console.log("clg is nice");
+
+//do while process
+
+// let i = 6, sum = 0;
+
+// do{
+//     sum += i;
+//     i++;
+// }
+// while (i>6 && i<8) 
+// console.log(sum);
+
+// let counter = 0;
+// for (let i = 0; i < 51; i++) {
+//     i % 10 === 0 ? counter += i : null
+//  };
+
+//  console.log(counter);
+
+// let brand ="Clarusway";
+// for (let i = 0; i < brand.length; i++) {
+//      console.log(i,brand[i])
+// };
+
+// let bests = ["Clarusway", "Google", "Amazon", "Tesla"];
+
+// for (let i = 0;i < bests.length; i++) {
+//     console.log(i+1,":", bests[i]);
+//     document.write("<h1>",i+1,":", bests[i], "</h1>")
+// }
+
+// let i = 0;
+// while (i< 10) {
+//     if ((i % 3 === 0) || i ===0){
+//         console.log(i);
+//     }
+//     i++;
+// }
+
+//  console.log(counter);
+
+// text = "clarusway"
+// for(let i = text.length; i > 0 ; i--){
+//     document.write(text[i-1])
+// }
+
+
+//NESTED LOOPS
+// for (let i = 0; i <= 10;i++){
+//     for(j=0; j<=10;j++){
+//         console.log(`${j} * ${i} = ${j * i}`);
+//     }
+// }
+
+// const list = [12,13,14,15]
+
+// let sum = 0
+// for (let i = 0; i < list.length ; i++) {
+//     sum += list[i]  
+// }
+// console.log(sum);
+
+
+// for (let i = 0; i <= 100 ; i++) {
+//     if (i%5===0  && i%3===0 ) {
+//         console.log("feez-buzz");
+//     }else if(i%5===0){
+//         console.log("feez");
+//     }else if(i%3===0){
+//         console.log("buzz");
+//     }else{
+//         console.log(i);
+//     }
+//     }
+        
